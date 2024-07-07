e2622ebc3ca35f68b107ef3294d1da160c3b71f2
akxcix <git@akxcix.com>
2024-07-06 23:39:33 -0400

    add instagram link

--- www.aeronode.io/src/common/constants.tsx
+++ www.aeronode.io/src/common/constants.tsx
@@ -1,4 +1,5 @@
 export const LINK_GITHUB = "https://github.com/koerolabs/aeronode"
 export const LINK_TWITTER = "https://twitter.com/koerolabs"
 export const LINK_DOCS = "https://docs.aeronode.io"
+export const LINK_INSTAGRAM = "https://instagram.com/koerolabs"
 
--- www.aeronode.io/src/components/footer.tsx
+++ www.aeronode.io/src/components/footer.tsx
@@ -1,4 +1,4 @@
-import { LINK_GITHUB, LINK_TWITTER } from "@/common/constants";
+import { LINK_GITHUB, LINK_INSTAGRAM, LINK_TWITTER } from "@/common/constants";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -10,7 +10,9 @@ export default function Footer() {
                         <div className="flex items-center gap-2 text-lg md:text-base">
                             <Link to={LINK_GITHUB} className="font-semibold hover:underline">github</Link>
                             |
-                            <Link to={LINK_TWITTER} className="font-semibold hover:underline">twitter</Link>                            
+                            <Link to={LINK_TWITTER} className="font-semibold hover:underline">twitter</Link>
+                            |
+                            <Link to={LINK_INSTAGRAM} className="font-semibold hover:underline">instagram</Link>                    
                         </div>
                     </div>
                 </div>
