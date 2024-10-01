16bc96b126130b65e16ed083beff992ce3bad37d
akxcix <git@akxcix.com>
2024-09-30 21:21:23 -0400

    update links to point to moonlitgarage

--- websites/aeronode.io/src/common/constants.tsx
+++ websites/aeronode.io/src/common/constants.tsx
@@ -1,6 +1,6 @@
-export const LINK_GITHUB = "https://github.com/koerolabs/aeronode"
-export const LINK_TWITTER = "https://twitter.com/koerolabs"
+export const LINK_GITHUB = "https://github.com/moonlitgarage/aeronode"
+export const LINK_TWITTER = "https://twitter.com/moonlitgarage"
 export const LINK_DOCS = "https://docs.aeronode.io"
-export const LINK_INSTAGRAM = "https://instagram.com/koerolabs"
+export const LINK_INSTAGRAM = "https://instagram.com/moonlit.garage"
 
 export const ENDPOINT_LOOPS = "https://app.loops.so/api/newsletter-form/clyp90ncf01hqmhc66dtx9yxa"
