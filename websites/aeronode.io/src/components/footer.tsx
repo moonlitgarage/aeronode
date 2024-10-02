85102866362d2af17d837b30db221a5010710229
akxcix <git@akxcix.com>
2024-10-02 04:10:50 -0400

    update footer

--- websites/aeronode.io/src/components/footer.tsx
+++ websites/aeronode.io/src/components/footer.tsx
@@ -1,18 +1,26 @@
-import { LINK_GITHUB, LINK_INSTAGRAM, LINK_TWITTER } from "@/common/constants";
+import { IMAGE_LOGO, LINK_MOONLITGARAGE } from "@/common/constants";
 import { Link } from "react-router-dom";
+import { useTheme } from '@/components/theme-provider';
+
 
 export default function Footer() {
+    const { theme } = useTheme();
+    const isDarkTheme = theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    
     return (
         <div id="header" className="mb-0">
             <footer className="sticky flex h-16 items-center gap-4 border-dashed border-t bg-background px-4 md:px-6">
                 <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                    <div className="ml-auto mr-auto flex-1 sm:flex-initial">
-                        <div className="flex items-center gap-2 text-lg md:text-base">
-                            <Link to={LINK_GITHUB} className="font-semibold hover:underline">github</Link>
-                            |
-                            <Link to={LINK_TWITTER} className="font-semibold hover:underline">twitter</Link>
-                            |
-                            <Link to={LINK_INSTAGRAM} className="font-semibold hover:underline">instagram</Link>                    
+                    <div className="ml-auto mr-auto flex-1 sm:flex-initial p-2">
+                        <div className="flex items-center gap-2 text-lg md:text-base font-semibold">
+                            <span>A Project By</span>
+                            <Link to={LINK_MOONLITGARAGE} className="hover:underline">
+                                <img 
+                                    src={IMAGE_LOGO} 
+                                    alt="Moonlit Garage logo" 
+                                    className={`mx-auto h-8 transition-all duration-300 ${isDarkTheme ? 'invert' : ''}`}
+                                />
+                            </Link>
                         </div>
                     </div>
                 </div>
--- websites/aeronode.io/src/routes/home-page.tsx
+++ websites/aeronode.io/src/routes/home-page.tsx
@@ -1,9 +1,9 @@
 import { LINK_DOCS, LINK_GITHUB } from "@/common/constants";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import ThreeDScence from "@/components/ui/threedscene";
 import { H1, H3, List } from "@/components/ui/typography";
 import { Link } from "react-router-dom";
+import AeronodeImage from "../../public/images/aeronode.png";
 
 export default function HomePage() {
     return (
@@ -21,8 +21,11 @@ export default function HomePage() {
                 </div>
                 <div className="p-12 flex flex-row">
                     <div className="grow"/>
-                    <div className="w-1/2 max-w-3xl aspect-video">
+                    {/* <div className="w-1/2 max-w-3xl aspect-video">
                         <ThreeDScence />
+                    </div> */}
+                    <div className="w-1/2 max-w-3xl">
+                        <img src={AeronodeImage} alt="AeroNode" className="w-full"/>
                     </div>
                     <div className="grow"/>
                 </div>
